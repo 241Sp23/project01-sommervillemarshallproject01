5bad663cc8818024574677d8fff76ce203130b5e
gagersommerville <gagersommerville@gmail.com>
2023-02-24 10:51:51 -0600

    Find method

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -1,17 +1,18 @@
 package project01;
+
 import java.util.*;
 import java.io.*;
 import javax.swing.*;
 
 public class Project01 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Setup to allow the user to choose the file
         JFileChooser myChooser = new JFileChooser();
         myChooser.showOpenDialog(null);
         File file = myChooser.getSelectedFile();
-        
-        
+        Scanner inputFind = new Scanner(file);
+
         Scanner input = new Scanner(System.in);
         System.out.println("What would you like to do.\nMenu (Choose one of the following or Q to quit):");
         System.out.println("F -Filter on Type");
@@ -21,7 +22,14 @@ public class Project01 {
         String choice = input.nextLine();
         System.out.print("\nWhat week would you like to search for (mm/dd/yyyy): ");
         String week = input.nextLine();
-        
+
+    }
+
+    public static String[][] find(Scanner fileInput) {
+
+        while (fileInput.hasNext()) {
+            String Date = fileInput.nextLine();
+        }
     }
 
 }
